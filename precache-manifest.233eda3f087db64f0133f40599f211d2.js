3430d621b066bd7fa4fed2f77310dfce6eb5df29
Josh Brearley <joshbrearley@live.com>
2020-03-30 15:38:30 +0100

    Updates

---
+++ precache-manifest.233eda3f087db64f0133f40599f211d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b865bb186e031e731f088a685887cd0",
+    "url": "/Testportfolio.git/index.html"
+  },
+  {
+    "revision": "7a4f912a0342fc1e0f3f",
+    "url": "/Testportfolio.git/static/css/2.ab965100.chunk.css"
+  },
+  {
+    "revision": "7d3d814f97fa82478a08",
+    "url": "/Testportfolio.git/static/css/main.deb48b17.chunk.css"
+  },
+  {
+    "revision": "7a4f912a0342fc1e0f3f",
+    "url": "/Testportfolio.git/static/js/2.350c4c70.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Testportfolio.git/static/js/2.350c4c70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3d814f97fa82478a08",
+    "url": "/Testportfolio.git/static/js/main.3e9acd91.chunk.js"
+  },
+  {
+    "revision": "05b76a062c2b338383fe",
+    "url": "/Testportfolio.git/static/js/runtime-main.86465843.js"
+  }
+]);
